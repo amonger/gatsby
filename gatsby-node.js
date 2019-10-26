2014a04e9fa97666387966e6e028b92466002264
Alan Monger <haveacigaro@gmail.com>
2019-10-26 14:54:58 +0100

    Add markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({boundActionCreators, graphql}) => {
+    const { createPage } = boundActionCreators
+    const postTemplate = path.resolve(`src/templates/blog-page.js`)
+
+    return graphql(`
+        query MyQuerys {
+            allMarkdownRemark {
+              edges {
+                node {
+                  frontmatter {
+                    title
+                    date
+                    path
+                  }
+                  excerpt
+                }
+              }
+            }
+          }
+    `).then(res => {
+        if (res.errors) {
+            return Promise.reject(res.errors)
+        }
+
+        res.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+                path: node.frontmatter.path,
+                component: postTemplate
+            })
+        })
+    })
+}
---
+++ src/templates/blog-page.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import { graphql } from "gatsby"
+
+const IndexPage = ({data}) => {
+	const post = data.markdownRemark
+	return <Layout>
+		<SEO title="Home" />
+		<h1>{post.frontmatter.title}</h1>
+		<div dangerouslySetInnerHTML={{ __html: post.html}} />
+	</Layout>
+}
+
+export const pageQuery = graphql`
+	query PostQuery ($path: String!) {
+		markdownRemark(frontmatter:{ path: { eq: $path}}) {
+			frontmatter {
+				title
+				date
+			}
+			html
+		}
+	}
+`
+
+export default IndexPage
